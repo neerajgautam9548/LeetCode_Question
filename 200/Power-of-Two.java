1cf90c47d62d5af738795f4dc889165013b753cb
neerajgautam9548 <neeraj.gautam_cs23@gla.ac.in>
2026-05-19 12:23:57 +0530

    [231] [Time Beats: 96.72%] [Memory Beats: 72.27%] - LeetPush

--- 200/Power-of-Two.java
+++ 200/Power-of-Two.java
@@ -1,13 +1,9 @@
 1class Solution {
 2    public boolean isPowerOfTwo(int n) {
-3        for(int i=0; i<31; i++){
-4            if((n>>i)%2==1){
-5                if(n==(1<<i)) return true;
-6                return false;
-7            }
-8 
-9        }
-10        return false;
-11    
-12    }
-13}
+3        if(n==0) return false;
+4        if(n==1) return true;
+5        if(n%2==1) return false;
+6        return isPowerOfTwo(n/2);
+7    
+8    }
+9}
