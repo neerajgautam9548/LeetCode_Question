5cc85af47cdf81c8289d929214de03b9eb7db7bc
neerajgautam9548 <neeraj.gautam_cs23@gla.ac.in>
2026-07-19 07:38:12 +0530

    [155] [Time Beats: N/A] [Memory Beats: N/A] - LeetPush

---
+++ 200/155-Min-Stack.java
@@ -0,0 +1,34 @@
+class MinStack {
+    int min=Integer.MAX_VALUE;
+    Stack<Integer> stack=new Stack<>();
+    public void push(int value) {
+        if(min>=value){
+            stack.push(min);
+            min=value;
+        }
+        stack.push(value);
+    }
+    
+    public void pop() {
+        if(stack.pop()==min){
+            min=stack.pop();
+        }
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(value);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
