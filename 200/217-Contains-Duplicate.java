757bdf1a358f0166f6232eb443057f54c29d985f
neerajgautam9548 <neeraj.gautam_cs23@gla.ac.in>
2026-07-19 12:42:15 +0530

    [217] [Time Beats: 5.08%] [Memory Beats: 5.19%] - LeetPush

---
+++ 200/217-Contains-Duplicate.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i:nums) map.put(i,map.getOrDefault(i,0)+1);
+        for(int i:map.values()){
+            if(i>1) return true;
+
+        }
+        return false;
+    }
+}
